ded186e4f4d5ca882988a8187fb32149238aa250
rubicon1990 <wei.yuxin@hotmail.com>
2020-10-12 11:05:29 +0800

    edited model select

--- python/model_select.py
+++ python/model_select.py
@@ -6,6 +6,8 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import ParameterGrid
 import datetime
 import os
+import gc
+import csv
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras.layers import Dropout, Dense, SimpleRNN, LSTM, GRU
@@ -26,20 +28,6 @@ def prepare_data(data_df, normalizer):
     norm_stock_price = normalize_data(sorted_stock_price, normalizer)
     data = extract_data(norm_stock_price, seq_len)
     return train_valid_test_split(data, valid_size_percent, test_size_percent)
-
-
-def train_model(model):
-
-    _model = model(h_dim, dropout_rate, output_dim)
-    _model.compile(optimizer=optimizer,
-                   loss="mse",
-                   metrics=["mae", "mape", "mse"])
-
-    return _model, _model.fit(x_train, y_train,
-                              batch_size=batch_size, epochs=n_epochs,
-                              shuffle=True,
-                              validation_split=validation_split,
-                              verbose=2)
 
 
 def rec_per_param(model):
@@ -73,14 +61,14 @@ def rec_per_param(model):
             test_mae, test_mape, test_mse]
 
 
-def save():
-    writer = pd.ExcelWriter(output_path, engine='xlsxwriter')
-
-    for nm in sorted(output.keys()):
-        res_df = pd.DataFrame(output[nm], columns=columns)
-        res_df.to_excel(writer, sheet_name=nm)
-
-    writer.save()
+# def save():
+#     writer = pd.ExcelWriter(output_path, engine='xlsxwriter')
+#
+#     for nm in sorted(output.keys()):
+#         res_df = pd.DataFrame(output[nm], columns=columns)
+#         res_df.to_excel(writer, sheet_name=nm)
+#
+#     writer.save()
 
 
 if __name__ == "__main__":
@@ -99,53 +87,87 @@ if __name__ == "__main__":
     input_path = '../input/prices-split-adjusted.csv'
     output_path = '../output/result_20201008.xlsx'
 
-    columns = ['h_dim', 'learning_rate',
+    columns = ['ticker', 'seq_len', 'h_dim', 'learning_rate',
                'train_loss', 'train_mae', 'train_mape', 'train_mse',
                'valid_loss', 'valid_mae', 'valid_mape', 'valid_mse',
                'test_mae', 'test_mape', 'test_mse']
 
-    # Load data
-    df = pd.read_csv(input_path, index_col=0)
     param_grad = {
         'ticker': ['EQIX', 'JPM', 'R', 'HES', 'COST'],
-        'seq_len': [21, 31, 45, 61],
-        'h_dim': [10, 15, 20, 25, 30],
+        'seq_len': [16, 21, 31],
+        'h_dim': [10, 15, 20, 25, 30, 50],
         'learning_rate': [0.001, 0.01, 0.1]
     }
 
     # param_grad = {
-    #     'ticker': ['EQIX', 'JPM'],
-    #     'seq_len': [45, 61],
-    #     'h_dim': [10, 15],
-    #     'learning_rate': [0.01]
+    #     'ticker': ['R', 'JPM'],
+    #     'seq_len': [31],
+    #     'h_dim': [15],
+    #     'learning_rate': [0.1]
     # }
 
-    output = {}
-    for p_dict in ParameterGrid(param_grad):
-        ticker = p_dict['ticker']
-        seq_len = p_dict['seq_len']
-        h_dim = p_dict['h_dim']
-        learning_rate = p_dict['learning_rate']
+    # Load data
+    df = pd.read_csv(input_path, index_col=0)
+    sc_norm = MinMaxScaler(feature_range=(0, 1))
 
-        sheet_nm = '{0}_{1}'.format(ticker, seq_len)
-        if sheet_nm not in output:
-            output[sheet_nm] = []
+    # output = {}
+    with open('../output/result_20201009.csv', 'a') as _f:
+        csv_write = csv.writer(_f)
+        csv_write.writerow(columns)
 
-        # Prepare data
-        sc_norm = MinMaxScaler(feature_range=(0, 1))
-        x_train, y_train, x_valid, y_valid, x_test, y_test = \
-            prepare_data(df, sc_norm)
+        for p_dict in ParameterGrid(param_grad):
+            ticker = p_dict['ticker']
+            seq_len = p_dict['seq_len']
+            h_dim = p_dict['h_dim']
+            learning_rate = p_dict['learning_rate']
 
-        # Model training
-        output_dim = y_train.shape[-1]
-        optimizer = tf.keras.optimizers.Adam(learning_rate)
-        nn_model, history = train_model(gru)
+            print('{0} - {1} - {2} - {3}'.format(ticker, seq_len, h_dim, learning_rate))
 
-        # Output per parameter
-        output[sheet_nm].append(rec_per_param(nn_model))
+            # sheet_nm = '{0}_{1}'.format(ticker, seq_len)
+            # if sheet_nm not in output:
+            #     output[sheet_nm] = []
+
+            # Prepare data
+            x_train, y_train, x_valid, y_valid, x_test, y_test = \
+                prepare_data(df, sc_norm)
+
+            # Model training
+            output_dim = y_train.shape[-1]
+            optimizer = tf.keras.optimizers.Adam(learning_rate)
+
+            tf.keras.backend.clear_session()
+            # nn_model = \
+            #     tf.keras.Sequential([GRU(h_dim, activation='tanh'),
+            #                          Dropout(dropout_rate),
+            #                          Dense(output_dim)])
+
+            # Construct and compile
+            inputs = tf.keras.Input(shape=(None, output_dim))
+            x = tf.keras.layers.GRU(h_dim, activation=tf.nn.tanh)(inputs)
+            x = tf.keras.layers.Dropout(dropout_rate)(x)
+            outputs = tf.keras.layers.Dense(output_dim)(x)
+
+            nn_model = tf.keras.Model(inputs, outputs)
+
+            nn_model.compile(optimizer=optimizer,
+                             loss="mse",
+                             metrics=["mae", "mape", "mse"])
+
+            history = nn_model.fit(x_train, y_train,
+                                   batch_size=batch_size, epochs=n_epochs,
+                                   shuffle=True,
+                                   validation_split=validation_split,
+                                   verbose=2)
+
+            # Output per parameter
+            # output[sheet_nm].append(rec_per_param(nn_model))
+            csv_write.writerow([ticker, seq_len] + rec_per_param(nn_model))
+
+            del nn_model
+            gc.collect()
 
     # Save output to sheet
-    save()
+    # save()
 
 
 
